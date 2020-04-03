fd5d1cd49db7ef480397ece620e985019f1153ce
Arkadyuti Bandyopadhyay <bandyopadhyayarkadyuti@gmail.com>
2020-04-03 19:57:20 +0530

    Modified styling for first page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+ constructor(props) {
+   super(props);
+   this.state = {
+     data: {}
+   }
+ }
+
+ render()
+  {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className="App">
+    <header>
+     <form id = "to-do-form">
+      <input type="text" id="heading">   
+      </input>
+      <select>
+       <option value="low">Low</option>
+       <option value="medium">Medium</option>
+       <option value="hard">Hard</option>
+      </select>
+      <button type="submit">Add</button>
+      <textarea></textarea>
+      <input type="date" id="date"></input>
+     </form>
+    </header>
+    <div>
     </div>
+   </div> 
   );
+}
 }
 
 export default App;
